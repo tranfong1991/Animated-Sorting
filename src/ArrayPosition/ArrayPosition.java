8cbb75af2ca7d18c6a804cc9cf3eaaf3f4e7575a
Andy Tran <tran_fong1991@yahoo.com>
2014-06-13 00:20:45 -0500

    Update ArrayPosition.java

--- src/ArrayPosition/ArrayPosition.java
+++ src/ArrayPosition/ArrayPosition.java
@@ -1,13 +1,13 @@
-package ArrayPosition;
+package Range;
 
 // Store the begin position and the end position of an array
 // Convenient for merge sort and quick sort 
 // because it just gives the positions in the same array instead of making sub arrays
-public class ArrayPosition {
+public class Range {
 	public int start;
 	public int end;
 
-	public ArrayPosition(int start, int end) {
+	public Range(int start, int end) {
 		this.start = start;
 		this.end = end;
 	}
